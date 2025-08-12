b0e3527c41eef9f70d2fefcc49f82e4fa3959fdf
LonelyLemon <rulu498@gmail.com>
2025-08-12 02:10:19 +0000

    nginx + env change

--- TORUM/frontend/src/services/api.ts
+++ TORUM/frontend/src/services/api.ts
@@ -8,7 +8,7 @@ import type { User, UserUpdate,
             } from "../types/index";
 
 export const api = axios.create ({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
 });
 
 api.interceptors.request.use(async (config) => {
